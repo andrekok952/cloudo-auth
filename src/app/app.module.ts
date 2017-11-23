06f227082f81d3e593ea3976a8eb2d5c8183e1ec
Kok <AndreKok@nedbank.co.za>
2017-11-23 15:45:00 +0200

    initial build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,38 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import {HttpModule} from '@angular/http';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { Todos } from '../providers/todos/todos';
+import { SignupPage } from '../pages/signup/signup';
+import { LoginPage } from '../pages/login/login';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage,
+    SignupPage
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage,
+    SignupPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    Todos
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController, AlertController } from 'ionic-angular';
+import { Todos } from '../../providers/todos/todos';
+import { LoginPage } from '../login/login';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,85 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  todos: any;
 
+  constructor(public nav: NavController, public todoService: Todos, public alertCtrl: AlertController) {
+
+  }
+
+  ionViewDidLoad(){
+
+    this.todoService.getTodos().then((data) => {
+      this.todos = data;
+    });
+
+  }
+
+  logout(){
+    this.todoService.logout();
+    this.todos = null;
+    this.nav.setRoot(LoginPage);
+  }
+
+  createTodo(){
+
+    let prompt = this.alertCtrl.create({
+      title: 'Add',
+      message: 'What do you need to do?',
+      inputs: [
+        {
+          name: 'title'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel'
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            this.todoService.createTodo({title: data.title});
+          }
+        }
+      ]
+    });
+
+    prompt.present();
+
+  }
+
+  updateTodo(todo){
+
+    let prompt = this.alertCtrl.create({
+      title: 'Edit',
+      message: 'Change your mind?',
+      inputs: [
+        {
+          name: 'title'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel'
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            this.todoService.updateTodo({
+              _id: todo._id,
+              _rev: todo._rev,
+              title: data.title
+            });
+          }
+        }
+      ]
+    });
+
+    prompt.present();
+  }
+
+  deleteTodo(todo){
+    this.todoService.deleteTodo(todo);
   }
 
 }
